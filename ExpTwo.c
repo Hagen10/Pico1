3b1305c3d84cf300163d32a8a5c7f17871f1a196
Hagen10 <simon_hagerup@hotmail.com>
2025-06-15 19:53:11 +0200

    Exp Two without interrupts

---
+++ ExpTwo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+
+#define PHOTO_TRANSISTOR 26
+
+int main()
+{
+    // Initialize the LED and button
+    gpio_init(PICO_DEFAULT_LED_PIN);
+    gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
+
+    adc_init();
+    adc_gpio_init(PHOTO_TRANSISTOR);
+    adc_select_input(0);
+
+    // Might need to configure it depending on how much light comes in to the room
+    uint16_t lightLevelSwitchAt = 50;
+
+    // Could this be achieved with an interrupt instead?
+
+    while (true) {
+        uint16_t result = adc_read();
+        if (result > lightLevelSwitchAt) gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        else gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        sleep_ms(1000);
+    }
+}
--- FirstPico.c
+++ FirstPico.c
@@ -1,24 +1,16 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
-#include "hardware/spi.h"
-#include "hardware/i2c.h"
-#include "hardware/dma.h"
-#include "hardware/pio.h"
-#include "hardware/interp.h"
-#include "hardware/timer.h"
-#include "hardware/watchdog.h"
-#include "hardware/clocks.h"
-#include "hardware/uart.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
 
-// My Stuff
-#define BUTTON_PIN 15
+#define PHOTO_TRANSISTOR 26
 
-void button_pressed(uint gpio, uint32_t event_mask) {
-    if (gpio == BUTTON_PIN && event_mask & GPIO_IRQ_EDGE_FALL)
-    {
-        gpio_xor_mask(1u << PICO_DEFAULT_LED_PIN);
-    }
-}
+// void button_pressed(uint gpio, uint32_t event_mask) {
+//     if (gpio == BUTTON_PIN && event_mask & GPIO_IRQ_EDGE_FALL)
+//     {
+//         gpio_xor_mask(1u << PICO_DEFAULT_LED_PIN);
+//     }
+// }
 
 int main()
 {
@@ -35,23 +27,27 @@ int main()
     // // You need to call this function at least more often than the 100ms in the enable call to prevent a reboot
     // watchdog_update();
 
-    // Starts at 
-    volatile bool led_status = 0;
     // Initialize the LED and button
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
 
-    gpio_init(BUTTON_PIN);
-    gpio_set_dir(BUTTON_PIN, GPIO_IN);
-    gpio_pull_down(BUTTON_PIN);  // Use internal pull-down resistor
+    adc_init();
+    adc_gpio_init(PHOTO_TRANSISTOR);
+    adc_select_input(0);
 
-    printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
-    printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
-    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
+    // gpio_init(BUTTON_PIN);
+    // gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    // gpio_pull_down(BUTTON_PIN);  // Use internal pull-down resistor
 
-    gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &button_pressed);
+    // gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &button_pressed);
+
+    // Might need to configure it depending on how much light comes in to the room
+    uint16_t lightLevelSwitchAt = 50;
 
     while (true) {
-        sleep_ms(10000);
+        uint16_t result = adc_read();
+        if (result > lightLevelSwitchAt) gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        else gpio_put(PICO_DEFAULT_LED_PIN, 1);
+        sleep_ms(1000);
     }
 }
