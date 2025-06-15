f9b2f64de1f05f6261920c538f648957f3b681b5
Hagen10 <simon_hagerup@hotmail.com>
2025-06-15 18:52:03 +0200

    cleanup experiment

--- ExpOne.c
+++ ExpOne.c
@@ -22,19 +22,6 @@ void button_pressed(uint gpio, uint32_t event_mask) {
 
 int main()
 {
-    // // Watchdog example code
-    // if (watchdog_caused_reboot()) {
-    //     printf("Rebooted by Watchdog!\n");
-    //     // Whatever action you may take if a watchdog caused a reboot
-    // }
-    
-    // // Enable the watchdog, requiring the watchdog to be updated every 100ms or the chip will reboot
-    // // second arg is pause on debug which means the watchdog will pause when stepping through code
-    // watchdog_enable(100, 1);
-    
-    // // You need to call this function at least more often than the 100ms in the enable call to prevent a reboot
-    // watchdog_update();
-
     // Starts at 
     volatile bool led_status = 0;
     // Initialize the LED and button
@@ -44,10 +31,6 @@ int main()
     gpio_init(BUTTON_PIN);
     gpio_set_dir(BUTTON_PIN, GPIO_IN);
     gpio_pull_down(BUTTON_PIN);  // Use internal pull-down resistor
-
-    printf("System Clock Frequency is %d Hz\n", clock_get_hz(clk_sys));
-    printf("USB Clock Frequency is %d Hz\n", clock_get_hz(clk_usb));
-    // For more examples of clocks use see https://github.com/raspberrypi/pico-examples/tree/master/clocks
 
     gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &button_pressed);
 
