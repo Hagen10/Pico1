0348520cc428bb94594842b0a0819e529d7e3973
Hagen10 <simon_hagerup@hotmail.com>
2025-07-01 09:29:42 +0200

    Enabling debug logs via USB

--- FirstPico.c
+++ FirstPico.c
@@ -5,28 +5,8 @@
 
 #define PHOTO_TRANSISTOR 26
 
-// void button_pressed(uint gpio, uint32_t event_mask) {
-//     if (gpio == BUTTON_PIN && event_mask & GPIO_IRQ_EDGE_FALL)
-//     {
-//         gpio_xor_mask(1u << PICO_DEFAULT_LED_PIN);
-//     }
-// }
-
 int main()
 {
-    // // Watchdog example code
-    // if (watchdog_caused_reboot()) {
-    //     printf("Rebooted by Watchdog!\n");
-    //     // Whatever action you may take if a watchdog caused a reboot
-    // }
-    
-    // // Enable the watchdog, requiring the watchdog to be updated every 100ms or the chip will reboot
-    // // second arg is pause on debug which means the watchdog will pause when stepping through code
-    // watchdog_enable(100, 1);
-    
-    // // You need to call this function at least more often than the 100ms in the enable call to prevent a reboot
-    // watchdog_update();
-
     // Initialize the LED and button
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
@@ -35,19 +15,19 @@ int main()
     adc_gpio_init(PHOTO_TRANSISTOR);
     adc_select_input(0);
 
-    // gpio_init(BUTTON_PIN);
-    // gpio_set_dir(BUTTON_PIN, GPIO_IN);
-    // gpio_pull_down(BUTTON_PIN);  // Use internal pull-down resistor
-
-    // gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &button_pressed);
-
     // Might need to configure it depending on how much light comes in to the room
     uint16_t lightLevelSwitchAt = 50;
 
+    // Could this be achieved with an interrupt instead?
+
+    // Needed for getting logs from `printf` via USB 
+    stdio_init_all();
+
     while (true) {
         uint16_t result = adc_read();
-        if (result > lightLevelSwitchAt) gpio_put(PICO_DEFAULT_LED_PIN, 0);
+        if (result < lightLevelSwitchAt) gpio_put(PICO_DEFAULT_LED_PIN, 0);
         else gpio_put(PICO_DEFAULT_LED_PIN, 1);
         sleep_ms(1000);
+        printf("TESTING AGAIN");
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 FirstPico.c is the file that is compiled and run on the Pico 1. Each solution to the Kitronik experiments will be moved over to separate file called ExpOne.c, ExpTwo.c etc.
+
+
+## Debugging
+
+CMakeLists.txt has `pico_enable_stdio_usb(FirstPico 1)` set to 1 to allow us to get logs from `printf` in our terminal. To disable this, set it to `0`.
+
+Then in your main you need the following code:
+
+```
+  // Needed for getting logs from `printf` via USB 
+    stdio_init_all();
+```
+
+To get logs, open a terminal and type `ls /dev/tty.usb*` which should give you a device such as `/dev/tty.usbmodem11401`. Note, that the device does not show up when in BOOTSEL mode. You need to flash the program on the microcontroller first, and then you can get logs. Afterwards, run `screen /dev/tty.usbmodem11401 115200` to receive logs in the terminal. 
